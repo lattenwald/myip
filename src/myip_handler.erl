a7e73a99d573a5590334dc68f06e83871ef48afb
Alexander Q <qalexx@gmail.com>
2022-03-29 16:33:27 +0300

    support x-real-ip

--- src/myip_handler.erl
+++ src/myip_handler.erl
@@ -5,8 +5,11 @@
 -export([init/2]).
 
 init(Req0, State) ->
-    #{peer := {Ip, _}} = Req0,
-    IpBin = unicode:characters_to_binary(lists:join($., lists:map(fun integer_to_binary/1, tuple_to_list(Ip)))),
+    #{headers := Headers, peer := {Ip, _}} = Req0,
+    IpBin = case maps:get(<<"x-real-ip">>, Headers, null) of
+                null -> unicode:characters_to_binary(lists:join($., lists:map(fun integer_to_binary/1, tuple_to_list(Ip))));
+                XRealIp -> XRealIp
+            end,
     Req = cowboy_req:reply(200,
         #{<<"content-type">> => <<"text/plain">>},
         IpBin,
